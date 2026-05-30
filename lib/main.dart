542b154c2a6a4960d8e9c11832847057689f5e76
Graham Williams <Graham.Williams@togaware.com>
2026-05-31 06:28:00 +1000

    autoLogin is now true by default.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 /// DiaryPod — private encrypted diary on your Solid Pod.
 ///
-// Time-stamp: <Thursday 2026-04-30 12:29:28 +1000 Graham Williams>
+// Time-stamp: <Sunday 2026-05-31 06:27:53 +1000 Graham Williams>
 ///
 /// Copyright (C) 2026, Togaware Pty Ltd
 ///
@@ -129,6 +129,13 @@ class _AppState extends State<_App> {
         image: const AssetImage('assets/images/app_image.jpg'),
         logo: const AssetImage('assets/images/app_icon.png'),
         link: 'https://github.com/gjwgit/diarypod',
+        clientId:
+            'https://solidcommunity.au/apps/diarypod/client-profile.jsonld',
+        redirectUris: [
+          'http://localhost:4400/redirect',
+          'com.togaware.todopod://redirect',
+          'https://solidcommunity.au/apps/diarypod/redirect.html',
+        ],
         child: const AppScaffold(),
       ),
     );
